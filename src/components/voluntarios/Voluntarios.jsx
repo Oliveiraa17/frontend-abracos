402f260ad92af6e545dc44989edb6144543925ec
Hugo-Ribeiro1 <hugorsousa44@gmail.com>
2024-11-04 15:11:36 -0300

    forms voluntários

--- src/components/voluntarios/Voluntarios.jsx
+++ src/components/voluntarios/Voluntarios.jsx
@@ -11,13 +11,13 @@ function Voluntarios() {
             <form className="voluntarios-formulario">
                 <h2>Informações pessoais:</h2>
                 <label>Nome completo:</label>
-                <input type="text" placeholder="Digite seu nome completo" />
+                <input type="text" placeholder="Digite seu nome completo" required/>
 
                 <label>Data de nascimento:</label>
-                <input type="date" />
+                <input type="date" required/>
 
                 <label>Endereço:</label>
-                <input type="text" placeholder="Digite seu endereço" />
+                <input type="text" placeholder="Digite seu endereço" required/>
 
                 <label>Telefone para contato:</label>
                 <input 
@@ -29,28 +29,28 @@ function Voluntarios() {
                 />
 
                 <label>E-mail:</label>
-                <input type="email" placeholder="Digite seu e-mail" />
+                <input type="email" placeholder="Digite seu e-mail" required/>
 
                 <h2>Formação e Experiência:</h2>
                 <label>Formação acadêmica (graduação, especializações):</label>
-                <input type="text" placeholder="Digite sua formação acadêmica" />
+                <input type="text" placeholder="Digite sua formação acadêmica" required />
 
                 <label>Experiência anterior em voluntariado (se houver):</label>
-                <input type="text" placeholder="Descreva sua experiência" />
+                <input type="text" placeholder="Descreva sua experiência" required/>
 
                 <h2>Disponibilidade</h2>
                 <label>Dias e horários disponíveis para atendimento:</label>
-                <input type="text" placeholder="Digite seus dias e horários disponíveis" />
+                <input type="text" placeholder="Digite seus dias e horários disponíveis" required/>
 
                 <label>Tipo de serviço que pode oferecer (atendimento individual, grupos, etc.):</label>
-                <input type="text" placeholder="Descreva o tipo de serviço" />
+                <input type="text" placeholder="Descreva o tipo de serviço" required/>
 
                 <h2>Outras Informações</h2>
                 <label>Áreas de interesse (crianças, adolescentes, adultos):</label>
-                <input type="text" placeholder="Digite suas áreas de interesse" />
+                <input type="text" placeholder="Digite suas áreas de interesse" required/>
 
                 <label>Razões para querer se voluntariar:</label>
-                <textarea placeholder="Descreva suas razões"></textarea>
+                <textarea placeholder="Descreva suas razões" required></textarea>
 
                 <button className='button_voluntario' type="submit">Enviar</button>
             </form>
